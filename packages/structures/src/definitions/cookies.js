a0c3db6ec7184181db5dfd343971761ec2e6f54d
Matthew Wall <matthew.wallt@gmail.com>
2019-11-13 19:33:34 -0500

    dynamic prop-type within graphql query/mutation

--- packages/structures/src/definitions/cookies.js
+++ packages/structures/src/definitions/cookies.js
@@ -1,28 +1,32 @@
 import PropTypes from 'prop-types';
 
-// Cookies Definition
-export default PropTypes.shape({
-  pooky: PropTypes.string.isRequired,
+// Dynamic Cookies Definition
+// JSON Stringified object
+export default PropTypes.objectOf(PropTypes.any);
 
-  pooky_owl: PropTypes.string.isRequired,
+// Non-Dynamic Strict PropType checker.. uncomment if you don't want to have a dynamic type.
+// export default PropTypes.shape({
+//   pooky: PropTypes.string.isRequired,
 
-  pooky_data: PropTypes.string.isRequired,
+//   pooky_owl: PropTypes.string.isRequired,
 
-  pooky_electric: PropTypes.string.isRequired,
+//   pooky_data: PropTypes.string.isRequired,
 
-  pooky_order_allow: PropTypes.string.isRequired,
+//   pooky_electric: PropTypes.string.isRequired,
 
-  pooky_performance: PropTypes.string.isRequired,
+//   pooky_order_allow: PropTypes.string.isRequired,
 
-  pooky_recaptcha: PropTypes.string.isRequired,
+//   pooky_performance: PropTypes.string.isRequired,
 
-  pooky_recaptcha_coherence: PropTypes.string.isRequired,
+//   pooky_recaptcha: PropTypes.string.isRequired,
 
-  pooky_settings: PropTypes.string.isRequired,
+//   pooky_recaptcha_coherence: PropTypes.string.isRequired,
 
-  pooky_telemetry: PropTypes.string.isRequired,
+//   pooky_settings: PropTypes.string.isRequired,
 
-  pooky_use_cookie: PropTypes.bool.isRequired,
+//   pooky_telemetry: PropTypes.string.isRequired,
 
-  updated_pooky_coherence: PropTypes.string.isRequired,
-});
+//   pooky_use_cookie: PropTypes.bool.isRequired,
+
+//   updated_pooky_coherence: PropTypes.string.isRequired,
+// });
